1ca2f51d4df67762522a3520ef89b2e0fedbea04
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-07-08 17:59:39 -0400

    wip

--- src/components/AllTimeStats/data.ts
+++ src/components/AllTimeStats/data.ts
@@ -1,6 +1,32 @@
 import { getAllTimeStatsJsonData } from "@/data/allTimeStatsJson";
 import type { IAllTimeStats } from "./AllTimeStats";
 import { getJsonStatYears } from "@/data/yearStatsJson";
+import type { IMostWatchedPerson } from "../Stats/MostWatchedPeople";
+
+const dateFormat = new Intl.DateTimeFormat("en-US", {
+  weekday: "short",
+  month: "short",
+  day: "numeric",
+  year: "numeric",
+  timeZone: "UTC",
+});
+
+function formatViewingDatesForMostWatchedPeople(
+  mostWatchedPeople: IMostWatchedPerson[],
+) {
+  return mostWatchedPeople.map((person) => {
+    return {
+      ...person,
+      viewings: person.viewings.map((viewing) => {
+        const viewingDate = new Date(viewing.date);
+        return {
+          ...viewing,
+          date: dateFormat.format(viewingDate),
+        };
+      }),
+    };
+  });
+}
 
 export async function getAllTimeStats(): Promise<{
   allTimeStats: IAllTimeStats;
@@ -12,6 +38,15 @@ export async function getAllTimeStats(): Promise<{
   return {
     allTimeStats: {
       ...allTimeStatsJsonData,
+      mostWatchedDirectors: formatViewingDatesForMostWatchedPeople(
+        allTimeStatsJsonData.mostWatchedDirectors,
+      ),
+      mostWatchedPerformers: formatViewingDatesForMostWatchedPeople(
+        allTimeStatsJsonData.mostWatchedPerformers,
+      ),
+      mostWatchedWriters: formatViewingDatesForMostWatchedPeople(
+        allTimeStatsJsonData.mostWatchedWriters,
+      ),
     },
     statYears,
   };
--- src/components/YearStats/data.ts
+++ src/components/YearStats/data.ts
@@ -1,8 +1,34 @@
 import type { IYearStats } from "./YearStats";
 import { getJsonStatYears, getJsonYearStats } from "@/data/yearStatsJson";
+import type { IMostWatchedPerson } from "../Stats/MostWatchedPeople";
 
 export function getStatYears() {
   return getJsonStatYears();
+}
+
+const dateFormat = new Intl.DateTimeFormat("en-US", {
+  weekday: "short",
+  month: "short",
+  day: "numeric",
+  year: "numeric",
+  timeZone: "UTC",
+});
+
+function formatViewingDatesForMostWatchedPeople(
+  mostWatchedPeople: IMostWatchedPerson[],
+) {
+  return mostWatchedPeople.map((person) => {
+    return {
+      ...person,
+      viewings: person.viewings.map((viewing) => {
+        const viewingDate = new Date(viewing.date);
+        return {
+          ...viewing,
+          date: dateFormat.format(viewingDate),
+        };
+      }),
+    };
+  });
 }
 
 export function getStatsForYear(year: string): {
@@ -15,6 +41,15 @@ export function getStatsForYear(year: string): {
   return {
     stats: {
       ...jsonYearStats,
+      mostWatchedDirectors: formatViewingDatesForMostWatchedPeople(
+        jsonYearStats.mostWatchedDirectors,
+      ),
+      mostWatchedPerformers: formatViewingDatesForMostWatchedPeople(
+        jsonYearStats.mostWatchedPerformers,
+      ),
+      mostWatchedWriters: formatViewingDatesForMostWatchedPeople(
+        jsonYearStats.mostWatchedWriters,
+      ),
     },
     statYears,
   };

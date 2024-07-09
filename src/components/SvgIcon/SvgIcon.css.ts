6029e732a422f12d9ae586223b6c32e319144ac9
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-07-09 14:31:06 -0400

    wip

--- src/components/SvgIcon/SvgIcon.css.ts
+++
@@ -1,8 +0,0 @@
-import { style } from "@vanilla-extract/css";
-import { foregroundColors } from "../../styles/colors.css";
-
-export const iconStyle = style({
-  fill: foregroundColors.subtle,
-  height: "auto",
-  width: "18px",
-});

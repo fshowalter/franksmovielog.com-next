7181824a5f3bd76e61af422f94a4ea68e2ae66d9
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-07-10 13:26:27 -0400

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // write your next.js configuration values.
   // output: "export",
 
